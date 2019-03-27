9f43b520e9b6fc41d83c602b63833185ae6406f5
Diego Marcelino <diego.arm.mar@gmail.com>
2019-03-27 15:20:56 -0300

    Fix paths

--- resources/models/get_models.py
+++ resources/models/get_models.py
@@ -1,16 +1,17 @@
 import bz2
 from glob import glob
+import os
 
 compressed_models_directory = '../../dependencies/dlib-models/*.bz2'
 compressed_models_paths = glob(compressed_models_directory)
-buffer_size = 10240 # 10k
+buffer_size = 50 * 1024 # 50k
 
 for file_path in compressed_models_paths:
-  file_name = file_path.split('/')[-1].replace('.bz2','')
+  file_name = file_path.split(os.sep)[-1].replace('.bz2','')
 
   with open(file_name, 'wb') as d, bz2.BZ2File(file_path, 'rb') as c:
     for data in iter(lambda: c.read(buffer_size), b''):
       d.write(data)
-  
+
   print("[MODEL]", file_name)
 
--- src/detector.py
+++ src/detector.py
@@ -3,15 +3,15 @@ import cv2
 import numpy as np
 from os import path
 
-predictor_path = 'models/shape_predictor_68_face_landmarks.dat'
-face_recognition_model_path = 'models/dlib_face_recognition_resnet_model_v1.dat'
+predictor_path = '../resources/models/shape_predictor_68_face_landmarks.dat'
+face_recognition_model_path = '../resources/models/dlib_face_recognition_resnet_model_v1.dat'
 
 face_detector = dlib.get_frontal_face_detector()
 points_detector = dlib.shape_predictor(predictor_path)
 face_recognition = dlib.face_recognition_model_v1(face_recognition_model_path)
 
 def get_face_descriptor(photo_name, scale=1):
-  photo = cv2.imread( path.join('images', photo_name))
+  photo = cv2.imread( path.join('../resources/images', photo_name))
   detected_faces = face_detector(photo, scale)
 
   if 1 != len(detected_faces):
